813497a72d35b9a2a13db9c0bbd3f3b9dec79abc
Blessing Nyagwaya <107154397+BlessingNyagwaya@users.noreply.github.com>
2023-03-23 10:49:22 +0200

    Update route.js
    Add path to folder with the static files

--- routers/route.js
+++ routers/route.js
@@ -1,6 +1,12 @@
 const path = require("path");
 const express = require(path.resolve('./') + "/node_modules/express");
 const router = express.Router();
+const cors = require('cors');
+
+//Middleware
+app.use(express.static('public'));
+app.use(cors());
+app.use(express.json());
 
 router.get('', async (req, res, next) => {
     res.addData(`From the root api: ${router.mdl.t('hello')}!`);
@@ -8,7 +14,7 @@ router.get('', async (req, res, next) => {
     return next();
 })
 
-router.get('/name', (req, res) => {
+router.get('/name', async (req, res) => {
     res.endWithData(`Hi, my name is ${router.Config('name')}`);
 
     return;
